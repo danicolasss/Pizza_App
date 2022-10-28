14e6cea6d7d45e3e86efd89d9a220bb1e9683787
dannynicolas.grangeo <danny-nicolas.grangeot@hotmail.com>
2022-10-28 08:43:56 +0200

    Ajoutez des fichiers projet.

---
+++ PizzaApp/extensions/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Core.dll;
+namespace PizzaApp.extensions
+{
+    public static class StringExtensions
+    {
+        public static string PremiereLettreMajuscule(this string str)
+        {
+            if (String.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+
+            // MONTAGNARDE (str)
+
+            string ret = str.ToLower();
+
+            // montagnarde (ret)
+
+            ret = ret.Substring(0, 1).ToUpper() + ret.Substring(1, ret.Length-1);
+            // 0123456789
+            // Montagnarde (ret)
+
+
+            return ret;
+        }
+    }
+}
